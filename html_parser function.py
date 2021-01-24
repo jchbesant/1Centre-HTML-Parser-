91144be3ea9b7fc20fa1d7c843379af6832495dc
jchbesant <77247580+jchbesant@users.noreply.github.com>
2021-01-24 17:01:09 +1300

    Update html_parser function.py

--- html_parser function.py
+++ html_parser function.py
@@ -8,13 +8,16 @@ followed.
 
 Assumed that self-closing tags are in the format <br/> like in the example.
 
-Also did not add a main() since I did not know the format it would be tested.
+I also did not want to import any Python modules which helped with HTML validating or parsing.
 
 """
 def html_parser(html_string):
     
     tags_list = []
+    
     sc_tags = []
+    
+    #Names of all the self-closing tags which will be disregarded in the programme
     sc_tag_names = ["area", "base", "br", "col", "embed", "hr", "img", "input" \
                     "link", "meta", "param", "source", "track", "wbr"]
     
@@ -23,6 +26,7 @@ def html_parser(html_string):
     valid = "Valid"
     invalid = "Invalid"
     
+    #Loop through the string to until all tags are found
     for i in html_string.split():
         tag_count = i.count("<")
         while tag_count != 0:
@@ -31,6 +35,7 @@ def html_parser(html_string):
 
             tag = i[i.find("<") + 1:i.find(">")]
             
+            #Conditions to check and match closing tags to open tags
             if tag[0] == "/":
                 if len(tags_list) > 0:
                     previous = tags_list[-1]
@@ -41,17 +46,29 @@ def html_parser(html_string):
                 else:
                     return invalid
                 
+            #Check and store self-closing tags - removed from validity check
             elif tag[-1] == "/" and tag[:-1] in sc_tag_names:
                 sc_tags.append(tag)
                 
+            #If tag is open add it to the list   
             else:
                 tags_list.append(tag)
-
+                
+            #Resest counter and i for iteration
             tag_count -= 1
             i = i[i.find(">") + 1:]
-    
+            
+    #Check if there are more tags left unmatched - if so return "Invalid"
     if len(tags_list) > 0:
         return invalid
 
     return valid
 
+def main():
+    """
+    test = html_parser('HTML document')
+    #print(test)
+    
+    """
+
+main()
